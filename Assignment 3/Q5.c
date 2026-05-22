5b426e5a6a2067270cfe1d13a3d1f665bf56b9a5
Mayur Ahire <mayurahire705@gmail.com>
2026-05-22 13:47:20 +0530

    Completed

---
+++ Assignment 3/Q5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+
+BOOL ChkVowel(char ch)
+{
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter Character: ");
+    scanf("%c",&cValue);
+
+    bRet = ChkVowel(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is Vowel");
+    }
+    else
+    {
+        printf("It is not Vowel");
+    }
+    return 0;
+}
