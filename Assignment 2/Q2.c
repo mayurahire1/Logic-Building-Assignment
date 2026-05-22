f5abb713a0d01ede871df1f2777e4a0a8460ffd4
Mayur Ahire <mayurahire705@gmail.com>
2026-05-22 11:55:06 +0530

    Completed

---
+++ Assignment 2/Q2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    // updater
+    if (iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while (iNo > 0)
+    {
+        printf("* ");
+        iNo--;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
