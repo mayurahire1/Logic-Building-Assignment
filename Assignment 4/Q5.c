4df4b31ab20a248724b178b6385776d2174b0777
Mayur Ahire <mayurahire705@gmail.com>
2026-05-26 00:50:00 +0530

    Completed

---
+++ Assignment 4/Q5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int FactDiff(int iNo)
+{
+    int iCnt = 0;
+    int iFactSum = 0;
+    int iNonFactSum = 0;
+    int iDiff = 0;
+
+    for(iCnt = 1; iCnt < iNo; iCnt++)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            iFactSum = iFactSum + iCnt;
+        }
+        else
+        {
+            iNonFactSum = iNonFactSum + iCnt;
+        }
+    }
+
+    iDiff = iFactSum - iNonFactSum;
+    
+    return iDiff;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &iValue);
+
+    iRet = FactDiff(iValue);
+
+    printf("%d\t",iRet);
+
+    return 0;
+}
